b9bd6131ffe4f04992e3bb8747914d87829bb77a
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-22 13:33:01 +0900

    Populate things migration

--- server/db/migrations/20220822032218_things.js
+++ server/db/migrations/20220822032218_things.js
@@ -2,14 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("things", function (table) {
+    table.increments("id").primary(); // primary key, serial
+    table.string("description", 32).notNullable();
+    table.dateTime("checked_in").notNullable().defaultTo(knex.fn.now()); // auto-generate date
+    table.boolean("archived").notNullable();
+    table.foreign("user_id").references("users.id"); // foreign key
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("things");
 };
