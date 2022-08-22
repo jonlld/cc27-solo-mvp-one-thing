9e15963313257b04b2e157edad751b199c4c4119
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-22 17:39:16 +0900

    Use refs to catch data from form inpus

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import classes from "./Login.module.css";
 
 function Login() {
+  // Refs
+  const nameInputRef = useRef();
+  const pswdInputRef = useRef();
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+
+    // using REFs to read the DOM directly
+    const enteredName = nameInputRef.current.value;
+    const enteredPswd = pswdInputRef.current.value;
+
+    console.log(enteredName, enteredPswd);
+  };
+
   return (
     <Card>
       <div className={classes["login-container"]}>
         <h1>One | Thing</h1>
-        <form className={classes["form-control"]}>
-          <label>Name</label>
-          <input type="text" placeholder="name"></input>
-          <label>Password</label>
-          <input type="password" placeholder="pswd"></input>
+        <form className={classes["form-control"]} onSubmit={loginHandler}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="name"
+            ref={nameInputRef}
+          ></input>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="pswd"
+            ref={pswdInputRef}
+          ></input>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </Card>
