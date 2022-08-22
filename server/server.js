48e8f7e2e8678ded4c37a47cfbf35c8be31d6258
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-22 12:00:44 +0900

    Add server.js and boilerplate

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+// for production or dev ports
+const PORT = process.env.PORT || 3030;
+
+const app = express();
+
+// ***** MIDDLEWARE *****
+
+app.use(cors()); // for front and back on localhost
+app.use(express.json()); // JSON
+app.use(express.urlencoded({ extended: true })); // form-encoded
+
+// ***** ROUTES *********
+
+// probably don't need controller / model here as small - keep logic here for now!
+
+// user
+app.post("/users/login", () => {}); // login user
+
+// thing
+app.get("/things", () => {}); // get data on active thing
+
+// ***** LISTEN ********
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
