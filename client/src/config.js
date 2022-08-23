f64484889c4d6e65004d969fd8afa60f23e36b33
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-23 21:29:29 +0900

    Switch config URL for demo

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://one-thing-server.herokuapp.com";
-export const API_URL = "http://localhost:3030";
+export const API_URL = "https://one-thing-server.herokuapp.com";
+// export const API_URL = "http://localhost:3030";
