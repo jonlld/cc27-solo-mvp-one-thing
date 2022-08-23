ebe2c49632fe59f7a2d4c2fbfc1290bf221fa5e4
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-23 09:14:21 +0900

    Start work on main.js

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,10 +1,30 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
+import classes from "./Main.module.css";
 
-const Main = () => {
+const greetings = ["Hi", "Go", "Yo", "Konnichiwa", "Hola"];
+const randomIndex = Math.floor(Math.random() * 5);
+const randomGreeting = greetings[randomIndex];
+
+const Main = ({ username, userId }) => {
+  const [taskIsActive, setTaskIsActive] = useState(false);
+  const [isCheckedIn, setIsCheckedIn] = useState();
+
+  // Card is a flex container
   return (
     <Card>
-      <h1>One | Thing</h1>
+      <div>
+        <h1>One | Thing</h1>
+        <p className={classes.greeting}>
+          {randomGreeting}, {username}!
+        </p>
+      </div>
+      <div>
+        <form>
+          <label>Choose your thing!</label>
+          <input type="text" placeholder="Today I will focus on..."></input>
+        </form>
+      </div>
     </Card>
   );
 };
