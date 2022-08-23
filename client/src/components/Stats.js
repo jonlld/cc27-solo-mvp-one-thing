72a55565acc4bb403de3037c0f9ce46b3b343473
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-23 11:22:21 +0900

    Add test rendering for stats view on button click

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -3,14 +3,31 @@ import Card from "../UI/Card";
 import classes from "./Stats.module.css";
 
 const Stats = () => {
-  return (
-    <Card>
-      <div>
-        <h1 className={classes.title}>All Done!</h1>
-        <button className={classes.stats}>View Stats</button>
-      </div>
-    </Card>
-  );
+  const [isViewStats, setIsViewStats] = useState(false);
+
+  const viewStatsHandler = (e) => {
+    e.preventDefault();
+    setIsViewStats(true);
+  };
+
+  if (!isViewStats) {
+    return (
+      <Card>
+        <div>
+          <h1 className={classes.title}>All Done!</h1>
+          <button className={classes.stats} onClick={viewStatsHandler}>
+            View Stats
+          </button>
+        </div>
+      </Card>
+    );
+  } else {
+    return (
+      <Card>
+        <h1>Test</h1>
+      </Card>
+    );
+  }
 };
 
 export default Stats;
